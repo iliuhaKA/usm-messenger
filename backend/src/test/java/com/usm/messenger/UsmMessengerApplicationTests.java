1edebd9bcbfecb7dace907a4366265bc9d86c5b4
iliuhaKA <iliuha.k.a@gmail.com>
2026-02-17 19:14:07 +0200

    Настроен бэкэнд проект Java + Spring Boot + Maven

---
+++ backend/src/test/java/com/usm/messenger/UsmMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usm.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsmMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
