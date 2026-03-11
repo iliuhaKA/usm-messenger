d49981fd891b89badfec339497f44c005c21cbfd
iliuhaKA <iliuha.k.a@gmail.com>
2026-03-11 15:36:42 +0200

    Были внесены общие правки по фиксам ошибок и налаживанию бд и App.tsx

--- backend/src/main/resources/db/migration/V1__init_schema.sql
+++ backend/src/main/resources/db/migration/V1__init_schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS USERS (
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(255),
-    role VARCHAR(20) NOT NULL DEFAULT "STUDENT",
+    role VARCHAR(20) NOT NULL DEFAULT 'STUDENT',
     avatar_url VARCHAR(500),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_seen TIMESTAMP,
@@ -27,8 +27,8 @@ CREATE TABLE IF NOT EXISTS chats_members (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
     chat_id BIGINT REFERENCES CHATS(id) ON DELETE CASCADE,
-    role VARCHAR(20) DEFAULT "MEMBER",
-    joined_at TIMESTAMP DEFAULT,
+    role VARCHAR(20) DEFAULT 'MEMBER',
+    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_pinned BOOLEAN DEFAULT FALSE,
     is_muted BOOLEAN DEFAULT FALSE,
     UNIQUE(user_id, chat_id)
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS messages (
     chat_id BIGINT REFERENCES chats(id) ON DELETE CASCADE,
     sender_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
     content TEXT,
-    type VARCHAR(20) DEFAULT "TEXT",
+    type VARCHAR(20) DEFAULT 'TEXT',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     reply_to_id BIGINT REFERENCES messages(id)
@@ -70,11 +70,11 @@ CREATE TABLE IF NOT EXISTS folders (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
     name VARCHAR(100) NOT NULL,
-    color VARCHAR(7) DEFAULT "#24513C"
+    color VARCHAR(7) DEFAULT '#24513C'
 );
 
 
-CREATE CREATE TABLE IF NOT EXISTS chats_folders (
+CREATE TABLE IF NOT EXISTS chats_folders (
     folder_id BIGINT REFERENCES folders(id) ON DELETE CASCADE,
     chat_id BIGINT REFERENCES chats(id) ON DELTE CASCADE,
     PRIMARY KEY (folder_id, chat_id)
@@ -87,5 +87,5 @@ CREATE INDEX idx_chat_members_user_id ON chats_members(user_id);
 CREATE INDEX idx_users_idnp ON users(idnp);
 
 INSERT INTO users (idnp, first_name, last_name, role, is_password_set)
-VALUES ('20011234567890', 'Admin', 'User', 'ADMIN', FALSE)
+VALUES ('2001123456789', 'Admin', 'User', 'ADMIN', FALSE)
 on CONFLICT DO NOTHING;
